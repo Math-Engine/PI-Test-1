92535b4e028c95e4a795a7f0b410c481a357cdf1
happy-spark <107300540+happy-spark@users.noreply.github.com>
2024-02-08 17:15:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ let result_3_qotn = [];
 for (var i = 0; i < 1000; i++) {
   temp = temp + Number(PI[i]);
   result.push(temp);
+  let ox = [0, 0];
   if (temp%3 == 0) {
     result_3_qotn.push("✅");
+    ox[0] = ox[0] + 1;
   }else{
     result_3_qotn.push("❌");
+    ox[1] = ox[1] + 1;
   }
 }
 
 execSync(`echo "${JSON.stringify(result)}" >> $GITHUB_STEP_SUMMARY`);
+execSync(`echo "<hr> ${<ul><li>✅: ${ox[0]}</li><li>❌: ${ox[1]}</li></ul>}" >> $GITHUB_STEP_SUMMARY`);
 execSync(`echo "<hr> ${JSON.stringify(result_3_qotn)}" >> $GITHUB_STEP_SUMMARY`);
