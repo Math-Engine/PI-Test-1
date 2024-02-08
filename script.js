e137c64e256710c5aa513a9fe0a7b9e849c71a27
happy-spark <107300540+happy-spark@users.noreply.github.com>
2024-02-08 17:03:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const execSync = require('child_process').execSync;
 const PI = fs.readFileSync(path.join(__dirname, `PI_1M.txt`), 'utf8');
 let result = [];
 let temp = 0;
+let result_3_qotn = [];
 
 for (var i = 0; i < 1000; i++) {
   temp = temp + Number(PI[i]);
   result.push(temp);
+  if (temp%3 == 0) {
+    result_3_qotn.push(":white_check_mark:");
+  }else{
+    result_3_qotn.push.push(":x:");
+  }
 }
 
 execSync(`echo "${JSON.stringify(result)}" >> $GITHUB_STEP_SUMMARY`);
+execSync(`echo "<hr> ${JSON.stringify(result_3_qotn)}" >> $GITHUB_STEP_SUMMARY`);
